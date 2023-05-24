76c3ee5085d2473dcb8ac746c6b0136252842b27
zambbo <rhkrqnwk1@naver.com>
2023-05-25 02:15:24 +0900

    modify preprocessing

--- MalILVR/preprocess.py
+++ MalILVR/preprocess.py
@@ -1,14 +1,25 @@
 import numpy as np
 import os
 
-arr = ["api_vectors", "benign", "malware"]
+SIZE=3514
+DATASET_PATH="./datasets/"
+BENIGN_PATH=os.path.join(DATASET_PATH, "benigns")
+MALWARE_PATH=os.path.join(DATASET_PATH, "malwares")
 
-def do(name):
-    a = os.listdir(f"./datasets/{name}")
+for path in [BENIGN_PATH, MALWARE_PATH]:
+    save_base_path = path + "_1d"
+    for file in os.listdir(path):
+        print(file, end="\r")
+        file_path = os.path.join(path, file)
 
-    for aa in a:
-        item = np.load(f"./datasets/{name}/" + aa).reshape(64,64,1)
-        np.save(f"./datasets/{name}/" + aa, item)
+        array = np.load(file_path)
+        
+        array = array.flatten()
+        array = array[:SIZE]
+        
+        save_file_path = os.path.join(save_base_path, file[:-4] + '_1d.npy')
+        np.save(save_file_path, array)
+        
 
-for name in arr:
-    do(name)
+
+
